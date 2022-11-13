fb4d971224f0e59299dd18a51cbf1573b23742da
peyman <peymanrahiii@gmail.com>
2022-11-13 02:41:30 -0800

    copy commit

--- forgetPassword-authentication/models/order.js
+++ forgetPassword-authentication/models/order.js
@@ -23,3 +23,15 @@ const orderSchema = new Schema({
 });
 
 module.exports = mongoose.model('Order', orderSchema);
+user: {
+  email: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }
+}
+});
